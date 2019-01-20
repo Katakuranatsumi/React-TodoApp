373b9b3fd6ece818dc6d6bcd7c1a779206807586
Katakuranatsumi <usako7233@gmail.com>
2019-01-20 19:38:23 +0900

    2018年1月20日Addボタンの追加

--- App.js
+++ App.js
@@ -1,59 +1,67 @@
 import React from 'react';
-import { Platform, StatusBar, StyleSheet, View, Text, ScrollView } from 'react-native';
+import { Platform, StatusBar, StyleSheet, View, Text, ScrollView, FlatList, TextInput, Button, KeyboardAvoidingView } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
 import AppNavigator from './navigation/AppNavigator';
+import { gray } from 'ansi-colors';
 
 // 高さの判断をして値を設定
 const STATUSBAR_HEIGHT = Platform.OS == 'ios' ? 20 : StatusBar.currentHeight;
 
 export default class App extends React.Component {
-  // state = {
-  //   isLoadingComplete: false,
-  // };
+
+  // コンストラクタを定義
+  constructor(props) {
+    super(props)
+    //  stateを初期化
+　  this.state = {
+    todo: [],
+    currentIndex: 0,
+    inputText: "",
+ }
+}
+　// TODOリストへの追加処理
+　onAddItem = () => {
+    const title = this.state.inputText
+    if (title == "") {
+     return
+    }
+    const index = this.state.currentIndex + 1
+    const newTodo = {index: index, title: title, done: false}
+    const todo = [...this.state.todo, newTodo]
+    this.setState({
+      todo: todo,
+      currentIndex: index,
+      inputText: ""
+    })
+  }
 
   render() {
       return (
-       <View style={styles.container}>
-         <View style={styles.filter}>
-           <Text></Text>
+       <KeyboardAvoidingView style={styles.container} behavior="padding">
+       <View>
+         <Text>Filterがここに配置されます。</Text>
          </View>
-         <ScrollView style={styles.todolist}>
-           <Text></Text>
-         </ScrollView>
-　       <View style={styles.input}>
-          <Text></Text>
-        </View>
-       </View>
+         <ScrollView style={styles.todolist}><FlatList data={this.state.todo}
+            renderItem={({item}) => <Text>{item.title}</Text>}
+            keyExtractor={(item, index) => "todo_" + item.index}
+            /></ScrollView>
+            <View style={styles.input}>
+            <TextInput
+             onChangeText={(text) => this.setState({inputText: text})}
+             value={this.state.inputText}
+             style={styles.inputText}
+           />
+           <Button 
+             onPress={this.onAddItem}
+             title="Add"
+             color="#841584"
+             style={styles.inputButton}
+           />
+           </View>
+       </KeyboardAvoidingView>
       );
     }
   }
-
-//   _loadResourcesAsync = async () => {
-//     return Promise.all([
-//       Asset.loadAsync([
-//         require('./assets/images/robot-dev.png'),
-//         require('./assets/images/robot-prod.png'),
-//       ]),
-//       Font.loadAsync({
-//         // This is the font that we are using for our tab bar
-//         ...Icon.Ionicons.font,
-//         // We include SpaceMono because we use it in HomeScreen.js. Feel free
-//         // to remove this if you are not using it in your app
-//         'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-//       }),
-//     ]);
-//   };
-
-//   _handleLoadingError = error => {
-//     // In this case, you might want to report the error to your error
-//     // reporting service, for example Sentry
-//     console.warn(error);
-//   };
-
-//   _handleFinishLoading = () => {
-//     this.setState({ isLoadingComplete: true });
-//   };
-// }
 
 const styles = StyleSheet.create({
   container: {
@@ -69,6 +77,14 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   input: {
-    height: 30 
+    height: 30,
+    flexDirection: 'row', 
   },
+  inputText: {
+    flex: 1,
+    backgroundColor: 'gray',
+  },
+  inputButton: {
+   width: 100, 
+  }
 });
