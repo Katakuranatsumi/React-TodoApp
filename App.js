74bba2b83b14193647d7953e3aada9a9e828ee38
Katakuranatsumi <usako7233@gmail.com>
2019-01-20 22:35:59 +0900

    2019年1月20日 AsyncStorageの追加

--- App.js
+++ App.js
@@ -1,24 +1,56 @@
 import React from 'react';
-import { Platform, StatusBar, StyleSheet, View, Text, ScrollView, FlatList, TextInput, Button, KeyboardAvoidingView } from 'react-native';
+import { Platform, StatusBar, StyleSheet, View, Text, ScrollView, FlatList, TextInput, Button, KeyboardAvoidingView, AsyncStorage } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
 import AppNavigator from './navigation/AppNavigator';
 import { gray } from 'ansi-colors';
 
 // 高さの判断をして値を設定
 const STATUSBAR_HEIGHT = Platform.OS == 'ios' ? 20 : StatusBar.currentHeight;
+// TODOを保持するKey/Valueのストアのキーを定義
+const TODO = "@todoapp.todo"
 
 export default class App extends React.Component {
 
   // コンストラクタを定義
   constructor(props) {
     super(props)
-    //  stateを初期化
+  //  stateを初期化
 　  this.state = {
     todo: [],
     currentIndex: 0,
     inputText: "",
  }
 }
+　// コンポーネントがマウントされた段階で読み込みを行う
+componentDidMount() {
+  this.loadTodo()
+}
+
+// AsyncStorageからTODOを読み込む処理
+loadTodo = async () => {
+  try {
+   const todoString = await AsyncStorage.getItem(TODO)
+   if (todoString) {
+    const todo = JSON.parse(todoString)
+    const currentIndex = todo.length
+    this.setState({todo: todo, currentIndex: currentIndex})
+   }
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+// AsyncStorageへTODOを保存する
+saveTodo = async (todo) => {
+  try{
+   const todoString = JSON.stringify(todo)
+   await AsyncStorage.setItem(TODO, todoString)
+  } catch (e) {
+   console.log(e)
+  }
+}
+
+
 　// TODOリストへの追加処理
 　onAddItem = () => {
     const title = this.state.inputText
@@ -33,6 +65,8 @@ export default class App extends React.Component {
       currentIndex: index,
       inputText: ""
     })
+  // saveTodoを読んで保存する
+　 this.saveTodo(todo)
   }
 
   render() {
